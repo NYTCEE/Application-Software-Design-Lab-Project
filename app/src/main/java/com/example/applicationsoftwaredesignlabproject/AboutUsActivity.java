cdf5abf8605dfac44e487a881d01742dd029e375
NYTCEE <nytcee@gmail.com>
2024-11-18 22:39:55 +0800

    All Runnable!

--- app/src/main/java/com/example/applicationsoftwaredesignlabproject/AboutUsActivity.java
+++ app/src/main/java/com/example/applicationsoftwaredesignlabproject/AboutUsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,51 +16,36 @@ public class AboutUsActivity extends AppCompatActivity {
 
         // 設定各個連結的點擊事件
         setupLinkClickListeners();
+
+        // 設定返回按鈕的點擊事件
+        Button backButton = findViewById(R.id.backButton);  // 新增的返回按鈕
+        backButton.setOnClickListener(v -> onBackPressed()); // 返回上一頁
     }
 
     private void setupLinkClickListeners() {
         // 設定網站連結點擊事件
         TextView websiteLink = findViewById(R.id.websiteLink);
-        websiteLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openUrl("https://linktr.ee/nytcee");
-            }
-        });
+        websiteLink.setOnClickListener(v -> openUrl("https://linktr.ee/nytcee"));
 
         // 設定 Email 連結點擊事件
         TextView emailLink = findViewById(R.id.emailLink);
-        emailLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
-                emailIntent.setData(Uri.parse("mailto:nytcee@gmail.com"));
-                try {
-                    startActivity(emailIntent);
-                } catch (Exception e) {
-                    // 處理沒有郵件應用程式的情況
-                    e.printStackTrace();
-                }
+        emailLink.setOnClickListener(v -> {
+            Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+            emailIntent.setData(Uri.parse("mailto:nytcee@gmail.com"));
+            try {
+                startActivity(emailIntent);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
 
         // 設定 YouTube 連結點擊事件
         TextView ytLink = findViewById(R.id.ytLink);
-        ytLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openUrl("https://www.youtube.com/channel/UC9tVhQFwEQ0V5AclzHj8q9Q");
-            }
-        });
+        ytLink.setOnClickListener(v -> openUrl("https://www.youtube.com/channel/UC9tVhQFwEQ0V5AclzHj8q9Q"));
 
         // 設定 Instagram 連結點擊事件
         TextView instagramLink = findViewById(R.id.instagramLink);
-        instagramLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openUrl("https://www.instagram.com/nytcee");
-            }
-        });
+        instagramLink.setOnClickListener(v -> openUrl("https://www.instagram.com/eenytc"));
     }
 
     // 開啟網頁的輔助函數
@@ -69,7 +55,6 @@ public class AboutUsActivity extends AppCompatActivity {
         try {
             startActivity(intent);
         } catch (Exception e) {
-            // 處理沒有瀏覽器應用程式的情況
             e.printStackTrace();
         }
     }
